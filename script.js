f1d97dee07a1b49750de028ad8c0760ba5accc59
neiverthgiovana <giovana.neiverth@escola.pr.gov.br>
2025-09-04 13:38:24 +0000

    aula 5

---
+++ script.js
@@ -0,0 +1,9 @@
+const avanca = document.querySelectorAll('.btn-proximo');
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo')
+        const proximoPasso = 'passo-' + this.getAtributte('data-proximo')
+    } )
+    atual.classlist.remove('ativo');
+    document.getElementById(proximoPasso).classlist.add('ativo');
+})
